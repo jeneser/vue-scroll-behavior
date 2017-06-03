74646bb3b9fc2d3b517c91a53f5e0cff27858673
jeneser <jeneserwang@gmail.com>
2017-06-03 21:38:58 +0800

    Build with rollup

---
+++ build/index.js
@@ -0,0 +1,52 @@
+var fs = require('fs')
+var path = require('path')
+var rollup = require('rollup')
+var babel = require('rollup-plugin-babel')
+var uglify = require('rollup-plugin-uglify')
+
+var version = process.env.VERSION || require('../package.json').version
+var author = process.env.VERSION || require('../package.json').author
+
+var banner =
+  '/**\n' +
+  ' * vue-scroll-behavior v' + version + '\n' +
+  ' * (c) ' + new Date().getFullYear() + ' \n' + author +
+  ' * @license MIT\n' +
+  ' */\n'
+
+rollup.rollup({
+    entry: path.resolve(__dirname, '..', 'src/vue-scroll-behavior.js'),
+    plugins: [
+      babel(),
+      uglify()
+    ]
+})
+.then(bundle => {
+    return write(path.resolve(__dirname, '../dist/vue-scroll-behavior.js'), bundle.generate({
+        format: 'umd',
+        moduleName: 'vueScrollBehavior'
+    }).code)
+})
+.then(() => {
+    console.log('Awsome! vue-scroll-behavior v' + version + ' builded.')
+})
+.catch(console.log)
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
+
+function write (dest, code) {
+  return new Promise(function (resolve, reject) {
+    code = banner + code
+    fs.writeFile(dest, code, function (err) {
+      if (err) return reject(err)
+      console.log(blue(dest) + ' ' + getSize(code))
+      resolve()
+    })
+  })
+}
--- src/vue-scroll-behavior.js
+++ src/vue-scroll-behavior.js
@@ -1,26 +1,31 @@
+/**
+  * vue-scroll-behavior v0.1.0
+  * (c) 2017 jeneser
+  * @license MIT
+  */
+
 import { getScrollTop, setScrollTop } from './utils/helpers'
 
 const vueScrollBehavior = {}
 
-// Plugin API
-// *********
+/**
+ * Plugin API
+ */
 vueScrollBehavior.install = function (Vue, opts) {
-  // Global property history list
+  // Global property
   Vue.historyList = []
 
   // Global method
   Vue.vueScrollBehavior = function (router) {
+
     if (typeof router !== 'undefined') {
-      // BeforeEach
+      // Router beforeEach
       router.beforeEach((to, from, next) => {
-        // Getting scrollTop
+
         let position = getScrollTop()
-        // Getting current path index
         let currentPathIndex = this.historyList.findIndex(e => {
           return e.path === from.fullPath
         })
-
-        console.log(currentPathIndex)
 
         if (currentPathIndex !== -1) {
           this.historyList[currentPathIndex].position = position
@@ -31,20 +36,19 @@ vueScrollBehavior.install = function (Vue, opts) {
           })
         }
 
-        console.log(this.historyList)
         next()
       })
 
-      // AfterEach
+      // Router afterEach
       router.afterEach(route => {
+
         let savedPosition = this.historyList.find(e => {
           return e.path === route.fullPath
         }) || 0
 
-        console.log(savedPosition)
-
         setScrollTop(savedPosition.position)
       })
+
     } else {
       console.warn('vue-scroll-behavior dependent on vue-router! ' +
         'Please Create the router instance.')
@@ -52,8 +56,9 @@ vueScrollBehavior.install = function (Vue, opts) {
   }
 }
 
-// Auto install
-// ********
+/**
+ * Auto install
+ */
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(vueScrollBehavior)
 }
